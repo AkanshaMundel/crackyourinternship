2229ef663c155943870468d2c7711947b1287f0c
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-07-19 17:38:15 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> r(numRows);
-        for(int i = 0;i< numRows;i++){
-            r[i].resize(i+1);
-            r[i][0] = r[i][i] = 1;
-            for(int j =1;j<i;j++){
-                r[i][j]= r[i-1][j-1] +r[i-1][j];
-            }
-        }
-        return r;
+        vector<vector<int>> ans(numRows);
+     for(int i= 0;i<numRows;i++){
+         ans[i].resize(i+1);
+         ans[i][0] = ans[i][i] = 1;
+         for(int j = 1;j<i;j++){
+            ans[i][j] =ans[i-1][j-1] +ans[i-1][j];
+         }
+           
+     }return ans;
+         
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+//     vector<vector<int>> generate(int numRows) {
+//         for(int i = 0; i<numRows; ++i)
+//         {
+//             vector<int> row(i+1, 1);
+//             for(int j = 1; j<i; ++j)
+//                 row[j] = ans[i-1][j] + ans[i-1][j-1];
+//             ans.push_back(row);
+//         }
+//         return ans;
+//     }
+    
+// };
