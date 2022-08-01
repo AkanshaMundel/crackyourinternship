26b4cc45c83da5fcf9cd7d00963a772047ded7b0
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-08-01 22:21:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    int fun(int m ,int n,vector<vector<int>>&dp){
-        if(m<0||n<0) return 0;
+//     int fun(int m ,int n,vector<vector<int>>&dp){
+//         if(m<0||n<0) return 0;
         
-        if(m==0 || n==0 ) return 1;
-        if(dp[m][n]!=-1) return dp[m][n];
-        int up=fun(m-1,n,dp);
-         int dwn=fun(m,n-1,dp);
+//         if(m==0 || n==0 ) return 1;
+//         if(dp[m][n]!=-1) return dp[m][n];
+//         int up=fun(m-1,n,dp);
+//          int dwn=fun(m,n-1,dp);
         
-        return dp[m][n]=(up+dwn);
-    }
+//         return dp[m][n]=(up+dwn);
+//     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fun(m-1,n-1,dp);
+         int dp[m][n];
+        
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i==0&&j==0 )dp[i][j] =1;
+                else{
+                  int up =0; 
+                int dwn = 0;
+                if(i>0) up=dp[i-1][j];
+               if(j>0)  dwn=dp[i][j-1];
+                dp[i][j]= up+dwn;  
+                }
+               
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
