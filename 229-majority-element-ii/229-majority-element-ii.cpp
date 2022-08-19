e4e17ff314e16015d2c55093d1d873e4e36c2eb5
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-08-19 12:51:32 +0530

    Time: 24 ms (40.57%), Space: 15.9 MB (52.94%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,16 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        vector<int> ans;
-        for(int i :nums){
-            mp[i]++;
-        }
-        for(auto i:mp){
-            if(i.second>nums.size()/3){
-                ans.push_back(i.first);
+       int n = nums.size();
+        int num1= -1;
+        int num2 = -1;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i =0;i<n;i++){
+            if(nums[i]==num1)cnt1++;
+            else if(nums[i]==num2){cnt2++;}
+            else if(cnt1==0){
+                num1= nums[i];
+                cnt1=1;
+                
+            }
+            else if(cnt2==0){
+                 num2= nums[i];
+                   cnt2=1;
+                      }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-        return ans;
+        //find the num1 and num2 now we just count no of time they apper in array 
+            vector<int> ans;
+             cnt2 =  cnt1=0;
+            for(int i =0;i<n;i++){
+                if(nums[i]==num1){
+                    cnt1++;
+                }
+                else if(nums[i]==num2){
+                      cnt2++;                        
+                      
+                }
+            }
+                if(cnt1>n/3){
+                    ans.push_back(num1);
+                }
+                if(cnt2>n/3){
+                    ans.push_back(num2);
+                }
+            
+            return ans;
+        
     }
 };
+//sc o(1)
