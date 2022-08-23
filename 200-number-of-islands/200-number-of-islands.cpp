7e9ac4a852f900d9356aca80537f49abd11352e5
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-08-23 18:22:10 +0530

    Time: 55 ms (52.76%), Space: 12.4 MB (55.20%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-    void bfs(int row,int col, vector<vector<char>>& grid,vector<vector<int>>&vis){
-        vis[row][col]=1;
-        queue<pair<int,int>>q;
-        q.push({row,col});
-        int n = grid.size();
+    void dfs(int i ,int j ,vector<vector<char>>& grid){
+      int n = grid.size();
         int m = grid[0].size();
+        if(i<0 || j<0 ||i>= n || j>=m) return ;
         
-         while(!q.empty()){
-             int row= q.front().first;
-             int col = q.front().second;
-             q.pop();
-             for( int delr = -1;delr<= 1;delr++){ //to traverse the in adj have four diection 
-                 for(int delc = -1;delc<= 1;delc++){
-                     if(!(delc==0 || delr==0))continue; 
-                     int nrow= row+delr;
-                     int ncol = col+delc;
-                     if(nrow>=0 && nrow<n && ncol >=0 && ncol <m &&  grid[nrow][ncol]=='1'&& !vis[nrow][ncol]){ //cheching 
-                        
-                            vis[nrow][ncol]=1;   
-                             q.push({nrow,ncol});  
-                         
-                     }
-                 }
-             }
-         }
+           if(grid[i][j]=='0') return ;
+           grid[i][j]='0' ;//making it zero so tht we cant visit it again 
+        
+            dfs(i-1,j,grid);
+            dfs(i,j-1,grid);
+            dfs(i,j+1,grid);
+            dfs(i+1,j,grid);
+           
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        int cnt =0;
-        for(int row = 0;row< n; row++){
-            for(int col = 0;col<m; col++){
-                if(!vis[row][col]&&grid[row][col]=='1'){
+        int cnt=0;
+        for(int i = 0;i<n ;i++){
+            for(int j = 0;j<m;j++){
+                if( grid[i][j]=='1'){
+                   
+                    dfs(i,j,grid);
                     cnt++;
-                    bfs(row,col,grid,vis);
+                    
                 }
-               
             }
         }
         return cnt;
