2d447907c8eb414b1d30309830c2ee9b7a79fda3
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-08-26 09:34:25 +0530

    Time: 28 ms (85.42%), Space: 13.5 MB (84.35%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    bool hascycle(int i,vector<vector<int>>& graph,vector<int>& color){
-            color[i]=1;
-        queue<int>q;
-        q.push(i);
-        while(!q.empty()){
-            int node= q.front();
-            q.pop();
-           
-            for(auto it: graph[node]){
-                if(color[it]==-1){
-                    color[it]= 1-color[node];
-                    q.push(it);
-                }
+    bool hascycle(int node,vector<vector<int>>& graph,vector<int>& color){
+               for(auto it: graph[node]){
+                    if(color[it]==-1){
+                        color[it]= 1-color[node];
+                        if(!hascycle(it,graph,color)){ return false;
+                    }
+                    }
                 else if(color[it]==color[node]){
                     return false;
                 }
-            }
-        }
+               }
+        
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        //BFS
+        //DFS
         int n = graph.size();
        vector<int> color(n, -1);
        // memset(color,-1,sizeof(color));
         for(int i =0;i<n;i++){
             if(color[i]==-1){
-                
+                color[i]=1;
                 if(!hascycle(i,graph,color))  return false;
                 
             }
