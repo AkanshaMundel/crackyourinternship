5832a6b728e20a93bb72f288f03e48fd8ea35a89
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-08-30 06:22:54 +0530

    Time: 1762 ms (7.98%), Space: 292.3 MB (8.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int fun(int ind,int prev,vector<int> &nums,int n, vector<vector<int>>&dp){
-        if(ind==n){
-            return 0;
-        }
-        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-        int len = 0+ fun(ind+1,prev,nums,n,dp);
+//     int fun(int ind,int prev,vector<int> &nums,int n, vector<vector<int>>&dp){
+//         if(ind==n){
+//             return 0;
+//         }
+//         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+//         int len = 0+ fun(ind+1,prev,nums,n,dp);
         
-        if(prev==-1||nums[ind]>nums[prev]){
-            len = max(len ,1+fun(ind+1,ind,nums,n,dp));
-        }
-        return dp[ind][prev+1]=len;
+//         if(prev==-1||nums[ind]>nums[prev]){
+//             len = max(len ,1+fun(ind+1,ind,nums,n,dp));
+//         }
+//         return dp[ind][prev+1]=len;
             
-    }
+//     }
     int lengthOfLIS(vector<int>& nums) {
 
-       // recrussion :
-        int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return fun(0,-1,nums,n,dp);
+//        // recrussion :
+//         int n = nums.size();
+//         vector<vector<int>>dp(n,vector<int>(n+1,-1));
+//         return fun(0,-1,nums,n,dp);
+   // TABULATION
+         int n = nums.size();
+         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    
+          for(int ind = n-1;ind>=0;ind--){
+             for( int prev= n-1;prev>=-1;prev--){
+               int len = 0+ dp[ind+1][prev+1];
+             if(prev==-1||nums[ind]>nums[prev]){
+                len = max(len ,1+dp[ind+1][ind+1]);
+                  }
+               dp[ind][prev+1]=len;
+                    }
+                }
+            return dp[0][-1+1];
     }
 };
