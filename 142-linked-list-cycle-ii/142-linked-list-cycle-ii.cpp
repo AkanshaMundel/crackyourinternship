a2f4f996f65785e9f308ff024c56bddf9e33071e
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-09-08 06:39:54 +0530

    Time: 18 ms (32.45%), Space: 10 MB (8.87%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -12,20 +12,31 @@ public:
         //using two pointer approach 
         //tc o(n)
         //sc o(1)
-        if(head==NULL || head->next ==NULL )return NULL;
-        ListNode *slow= head;
-        ListNode *fast = head;
-         ListNode *entry= head;
-        while(fast ->next && fast->next->next ){ //CHECKING IF THER IS COLLOSION POINT 
-            slow = slow->next;
-            fast = fast->next->next;
-            if( slow ==fast){            //IF HAVE THEN CHEKING THE CYCLE 
-                while(entry !=slow){
-                    slow= slow->next;
-                      entry= entry ->next;
-                     }
-                return entry;
-               }
+        // if(head==NULL || head->next ==NULL )return NULL;
+        // ListNode *slow= head;
+        // ListNode *fast = head;
+        //  ListNode *entry= head;
+        // while(fast ->next && fast->next->next ){ //CHECKING IF THER IS COLLOSION POINT 
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        //     if( slow ==fast){            //IF HAVE THEN CHEKING THE CYCLE 
+        //         while(entry !=slow){
+        //             slow= slow->next;
+        //               entry= entry ->next;
+        //              }
+        //         return entry;
+        //        }
+        // }
+        // return NULL;
+        // using hash map tc = sc = O(n)
+        unordered_map<ListNode*,int>mp;
+        while(head){
+            if(mp.find(head)!=mp.end())return head; //has cycle
+            else{
+                mp[head]= 1;
+                head= head->next;
+                
+            }
         }
         return NULL;
     }
