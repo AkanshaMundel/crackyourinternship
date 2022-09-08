6e1aa12fb07bf4cc20a8cda034aaa1f606d5b21c
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-09-08 06:30:17 +0530

    Time: 6 ms (95.27%), Space: 7.5 MB (75.30%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        //using two pointer approach 
+        //tc o(n)
+        //sc o(1)
+        if(head==NULL || head->next ==NULL )return NULL;
+        ListNode *slow= head;
+        ListNode *fast = head;
+         ListNode *entry= head;
+        while(fast ->next && fast->next->next ){ //CHECKING IF THER IS COLLOSION POINT 
+            slow = slow->next;
+            fast = fast->next->next;
+            if( slow ==fast){            //IF HAVE THEN CHEKING THE CYCLE 
+                while(entry !=slow){
+                    slow= slow->next;
+                      entry= entry ->next;
+                     }
+                return entry;
+               }
+        }
+        return NULL;
+    }
+};
