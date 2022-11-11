e7b8c755cd5961cdae104c69509fb7206e70f3bf
Akansha Mundel <78690652+AkanshaMundel@users.noreply.github.com>
2022-11-11 15:14:54 +0530

    Added solution - LeetHub

--- Primes sum - GFG/primes-sum.cpp
+++ Primes sum - GFG/primes-sum.cpp
@@ -8,25 +8,37 @@ using namespace std;
 class Solution {
 public:
     string isSumOfTwo(int N){
+        vector<bool>primes(N+1,1);
+        primes[0]=0, primes[1]=0;
+        for(int i = 2; i*i<=N;i++){
+            if(primes[i]==1){
+                for(int j = i*i;j<=N; j +=i){
+                    primes[j]= 0;
+                }
+            }
+        }
+        
+        
         // code here 
-        for(int i = 2; i<N; i++){ //just moving till n/2 coz sum is near ab n/2 
-            if(prime(i)&& prime (N-i)){
+        for(int i = 2; i<=N; i++){ //just moving till n/2 coz sum is near ab n/2 
+            if(primes[i]&& primes[N-i]){
                 return "Yes";
             }
            
         }
         return "No";
     }
-    bool prime(int n ){
-        for(int i = 2 ; i <= sqrt(n); i++){
-            if(n %i ==0) {
-                return false;
-            }
+    // bool prime(int n ){
+    //     for(int i = 2 ; i <= sqrt(n); i++){
+    //         if(n %i ==0) {
+    //             return false;
+    //         }
            
-        }
-         return true;
-    }
+    //     }
+    //      return true;
+    // }
 };
+//tc  n 
 
 //{ Driver Code Starts.
 int main() {
